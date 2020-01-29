5ee7ad7fbb0b366e7f19204be37bcca826a3e090
radek <radizd@gmail.com>
2020-01-29 22:37:12 +0100

    add many to many relation for category and product

--- src/main/java/pl/erti/shopping/shoppingapi/dao/domain/CategoryEntity.java
+++ src/main/java/pl/erti/shopping/shoppingapi/dao/domain/CategoryEntity.java
@@ -3,17 +3,18 @@ package pl.erti.shopping.shoppingapi.dao.domain;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.Set;
 
 @Getter
 @Setter
 @Entity
+@Table(name = "category")
 public class CategoryEntity {
     @Id
     private Long id;
     @Column
     private String name;
+    @ManyToMany(mappedBy = "categories")
+    private Set<ProductEntity> products;
 }
--- src/main/java/pl/erti/shopping/shoppingapi/dao/domain/ProductEntity.java
+++ src/main/java/pl/erti/shopping/shoppingapi/dao/domain/ProductEntity.java
@@ -3,13 +3,13 @@ package pl.erti.shopping.shoppingapi.dao.domain;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Set;
 
 @Getter
 @Setter
 @Entity
+@Table(name = "product")
 public class ProductEntity {
     @Id
     private Long id;
@@ -17,4 +17,11 @@ public class ProductEntity {
     private String name;
     @Column
     private String description;
+    @ManyToMany
+    @JoinTable(
+            name = "product_category",
+            joinColumns = @JoinColumn(name = "product_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id")
+    )
+    private Set<CategoryEntity> categories;
 }
